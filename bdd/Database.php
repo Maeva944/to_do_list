4c0678b83d46b6419c0af25ef0b21dbc0515377a
Maeva Nono <maeva.nono@efrei.net>
2025-01-03 22:09:33 +0100

    reglage bdd

---
+++ bdd/Database.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Database{
+    private $host= "localhost";
+    private $dbname = "todolist";
+    private $user = "root";
+    private $password = "";
+    private $charset = "utf8bm4";
+    public $pdo;
+
+    public function __construct(){
+        $dsn = "mysql:host={$this->host}; dbname={$this->dbname}; charset={$this->charset}"; 
+        try{
+            $this->pdo = new PDO($dsn, $this->user, 
+            $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION).
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e){
+            throw("Il y'a eux une erreur lors de la connexion avec la base de données :". $e->getMessage());
+        }
+    }
+}
